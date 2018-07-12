9c2c669b5a0e8dc4de1b2167505a983e0d30a49e
Cuciureanu Dragos <dragos_bv11@yahoo.com>
2018-07-12 10:12:32 +0200

    added the type of text/javascript in all the files at the scri[t tag

--- votes/js.js
+++ votes/js.js
@@ -1,6 +1,9 @@
+/* STILL HAVE TO WORK ON IT, THE PROBLEM IS THAT IT DOES NOT WORK FOR 
+   NUMBERS LIKE 99 -> 100 , 9 -> 10, different length numbers */
+
 const getEl = (el,all=false) => all?document.querySelectorAll(el):document.querySelector(el);
 
-let votes = 35;
+let votes = 35; 
 
 const wrapper = getEl('#wrapper');
 const moreVotesHolder = getEl('.more_votes');
@@ -10,11 +13,14 @@ const upArrow = getEl('.icon-up');
 const downArrow = getEl('.icon-down');
 const allSpans = getEl('span',true);
 
-let votedFor = 0; //1 up 0 neutral -1 down
+let votedFor = 0; //Keep track what the user has voted for (values: 1 / 0 / -1)
 
+
+/* EVENT LISTENERS */
 upArrow.addEventListener('click',upListener);
 downArrow.addEventListener('click',downListener);
 
+/* Add a new class to an array of elements, cleaning the old one */
 const cleanAndAdd = (elements,clas) => {
   elements.forEach(el=>{
       el.classList = '';
@@ -22,6 +28,7 @@ const cleanAndAdd = (elements,clas) => {
   })
 }
 
+/* Function called when the user change his vote */
 const changeStatus = (to)=>{
    if(to==''){
      wrapper.classList = ''
@@ -31,6 +38,7 @@ const changeStatus = (to)=>{
    
 }
 
+/* Populate the dom with the number of votes/number of votes + 1 / number of votes - 1 */
 const populate = (n,el) => {
   el.innerHTML = '';
   String(n).split('').forEach(number=>{
@@ -39,40 +47,43 @@ const populate = (n,el) => {
     el.appendChild(span);
   })
 }
-
 populate(votes , actualVotesHolder)
 populate(votes-1 , lessVotesHolder)
 populate(votes+1 , moreVotesHolder)
 
+
+/*Function that get called when the upArrow is beig pressed*/
 function upListener(){
-  /*check the diferences between the actual num and the coming one */
-  if(votedFor==1){
+  
+  if(votedFor==1){ //if the user has alredy voted for Up , restore to neutral
     votedFor = 0;
     [...getEl('span',true)].forEach(el=>el.classList='')
     changeStatus('');
     return
   }
+  //Change the status of the app and animate
   changeStatus('up');
+  //Get each number span from all the holders
   let upNumbers = getEl('.more_votes span' , true)
   let actualNumbers = getEl('.actual_votes span', true)
   let lessVotes = getEl('.less_votes span' , true)
-  /*must do something for case like 100 -> 99 */
+  
   
   upNumbers.forEach((el,idx,arr)=>{
-    let thisUpperNumber = el.innerHTML;
-    
-    if(thisUpperNumber != String(votes)[idx]){
+    //if this peace of number / span is not equal with the one that has to be replaced with then animate , if not , do nothing
+    if(el.innerHTML != String(votes)[idx]){
       cleanAndAdd([el,actualNumbers[idx],lessVotes[idx]],'transition-more')
     }
+    //fake callback, at the end of the forEach, if the last vote was negative, upp the number by 2 if not just by 1
     if(idx==arr.length-1){
       votes += (votedFor==-1)?2:1
       votedFor = 1;
     }
   })
-  
 }
+
+
 function downListener(){
-/*check the diferences between the actual num and the coming one */
   if(votedFor == -1){
     votedFor = 0;
     [...getEl('span',true)].forEach(el=>el.classList='')
@@ -83,12 +94,11 @@ function downListener(){
   let upNumbers = getEl('.more_votes span' , true)
   let actualNumbers = getEl('.actual_votes span', true)
   let lessNumbers = getEl('.less_votes span' , true)
-  /*must do something for case like 100 -> 99 */
+  
   changeStatus('down');
   lessNumbers.forEach((el,idx,arr)=>{
-    let thisUpperNumber = el.innerHTML;
-    
-    if(thisUpperNumber != String(votes)[idx]){
+
+    if(el.innerHTML != String(votes)[idx]){
       cleanAndAdd([el,actualNumbers[idx],upNumbers[idx]],'transition-less')
     }
     
